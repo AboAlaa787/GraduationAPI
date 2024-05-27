13e136b3082844e11c5b62c4d7839ea47b9af621
Haidar Jaded <haidar.king787@gmail.com>
2024-05-27 12:31:55 +0300

    Adding many permissions to client at once

--- app/Http/Controllers/PermissionClientController.php
+++ app/Http/Controllers/PermissionClientController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client;
+use App\Models\Permission;
 use App\Models\Permission_client;
 use App\Traits\ApiResponseTrait;
 use App\Traits\CRUDTrait;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
+use InvalidArgumentException;
 
 /**
  * @group Permissions_Clients management
@@ -46,17 +52,58 @@ class PermissionClientController extends Controller
 
     /**
      * @param Request $request
+     * @bodyParam permissions_ids integer[] permissions numbers to be added to the client.
      * @return JsonResponse
-     */
+ */
     public function store(Request $request): JsonResponse
     {
-        $validation = Validator::make($request->all(), ['permission_id' => 'required|exists:permissions,id|unique:permission_clients,permission_id,NULL,id,client_id,' . $request->input('client_id'), 'client_id' => 'required|exists:clients,id|unique:permission_clients,client_id,NULL,id,permission_id,' . $request->input('permission_id')]);
+        $validation = Validator::make($request->all(), ['permissions_ids' => 'required|array'
+            , 'client_id' => 'required|exists:clients,id'
+        ]);
         if ($validation->fails()) {
-            return $this->apiResponse($validation->messages(), 404, 'Failed');
+            return $this->apiResponse($validation->messages(), 422, 'Failed');
         }
-        return $this->store_data($request, new Permission_client());
+        try {
+            $this->authorize('create', Permission_client::class);
+            $permissionsIds = $request->get('permissions_ids', []);
+            $clientId = $request->get('client_id');
+            if (empty($permissionsIds)) {
+                return $this->apiResponse(null, 422, 'Error: At least one of permissions_ids is required.');
+            }
+
+            return DB::transaction(function () use ($permissionsIds, $clientId) {
+                $client = Client::findOrFail($clientId);
+                $this->attachPermissionsToClient($permissionsIds, $client);
+                $client->load(['permissions']);
+                return $this->apiResponse($client);
+            });
+        } catch (InvalidArgumentException $exception) {
+            return $this->apiResponse(null, 400, 'Error: ' . $exception->getMessage());
+
+        } catch (ModelNotFoundException $exception) {
+            $model = explode('\\', $exception->getModel());
+            $model = end($model);
+            $id = $exception->getIds()[0];
+            return $this->apiResponse(null, 404, "Error: $model with ID $id not found.");
+
+        } catch (AuthorizationException $exception) {
+            return $this->apiResponse(null, 403, 'Error: ' . $exception->getMessage());
+        }
     }
 
+    protected function attachPermissionsToClient($permissionsIds, $client): void
+    {
+        if (!is_array($permissionsIds)) {
+            throw new InvalidArgumentException('permissions_ids must be an array.');
+        }
+        foreach ($permissionsIds as $permissionId) {
+            $permission = Permission::findOrFail($permissionId);
+            if ($client->Permissions->contains($permission->id)) {
+                throw new InvalidArgumentException('Error: id ' . $permissionId . ' in permissions_ids is already attached to the client.');
+            }
+            $client->Permissions()->attach($permissionId);
+        }
+    }
     /**
      * @param $id
      * @return JsonResponse
