10d0aad140f4027e13fba08c70f9386761c0cfd7
Haidar Jaded <haidar.king787@gmail.com>
2024-05-27 12:42:15 +0300

    Adding many permissions to user at once

--- app/Http/Controllers/PermissionUserController.php
+++ app/Http/Controllers/PermissionUserController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Permission;
 use App\Models\Permission_user;
+use App\Models\User;
 use App\Traits\ApiResponseTrait;
 use App\Traits\CRUDTrait;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
+use InvalidArgumentException;
 
 /**
  * @group Permissions_Users management
@@ -46,15 +52,58 @@ class PermissionUserController extends Controller
 
     /**
      * @param Request $request
+     * @bodyParam permissions_ids integer[] permissions numbers to be added to the user.
      * @return JsonResponse
+     * @throws AuthorizationException
      */
     public function store(Request $request): JsonResponse
     {
-        $validation = Validator::make($request->all(), ['permission_id' => 'required|exists:permissions,id|unique:permission_users,permission_id,NULL,id,user_id,' . $request->input('user_id'), 'user_id' => 'required|exists:users,id|unique:permission_users,user_id,NULL,id,permission_id,' . $request->input('permission_id')]);
+        $validation = Validator::make($request->all(), ['permissions_ids' => 'required|array'
+            , 'user_id' => 'required|exists:users,id'
+        ]);
         if ($validation->fails()) {
-            return $this->apiResponse($validation->messages(), 404, 'Failed');
+            return $this->apiResponse($validation->messages(), 422, 'Failed');
         }
-        return $this->store_data($request, new Permission_user());
+        try {
+            $this->authorize('create', Permission_user::class);
+            $permissionsIds = $request->get('permissions_ids', []);
+            $userId = $request->get('user_id');
+            if (empty($permissionsIds)) {
+                return $this->apiResponse(null, 422, 'Error: At least one of permissions_ids is required.');
+            }
+
+            return DB::transaction(function () use ($permissionsIds, $userId) {
+                $user = User::findOrFail($userId);
+                $this->attachPermissionsToClient($permissionsIds, $user);
+                $user->load(['permissions']);
+                return $this->apiResponse($user);
+            });
+        } catch (InvalidArgumentException $exception) {
+            return $this->apiResponse(null, 400, 'Error: ' . $exception->getMessage());
+
+        } catch (ModelNotFoundException $exception) {
+            $model = explode('\\', $exception->getModel());
+            $model = end($model);
+            $id = $exception->getIds()[0];
+            return $this->apiResponse(null, 404, "Error: $model with ID $id not found.");
+
+        } catch (AuthorizationException $exception) {
+            return $this->apiResponse(null, 403, 'Error: ' . $exception->getMessage());
+        }
+    }
+
+    protected function attachPermissionsToClient($permissionsIds, $user): void
+    {
+        if (!is_array($permissionsIds)) {
+            throw new InvalidArgumentException('permissions_ids must be an array.');
+        }
+        foreach ($permissionsIds as $permissionId) {
+            $permission = Permission::findOrFail($permissionId);
+            if ($user->Permissions->contains($permission->id)) {
+                throw new InvalidArgumentException('Error: id ' . $permissionId . ' in permissions_ids is already attached to the user.');
+            }
+            $user->Permissions()->attach($permissionId);
+        }
     }
 
     /**
