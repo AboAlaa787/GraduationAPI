6d85c6b280d2e9b4c6533d06a4b9501acbab6e10
Haidar Jaded <haidar.king787@gmail.com>
2024-06-23 00:20:04 +0300

    Fixng a bug

--- app/Models/Devices_orders.php
+++ app/Models/Devices_orders.php
@@ -42,12 +42,11 @@ class Devices_orders extends Model
                     $device->date_receipt = now();
                     $device->save();
                 }
-                $undeliveredDevicesCount = $order->devices_orders()->where('deliver_to_client', false)->where('deliver_to_user', false)->count();
-                $undeliveredProductsCount = $order->products_orders()->where('deliver_to_client', false)->where('deliver_to_user', false)->count();
+                $undeliveredDevicesCount = $order->devices_orders()->where('deliver_to_client', false)->orWhere('deliver_to_user', false)->count();
+                $undeliveredProductsCount = $order->products_orders()->where('deliver_to_client', false)->orWhere('deliver_to_user', false)->count();
                 if ($undeliveredDevicesCount === 0 && $undeliveredProductsCount === 0) {
                     $order->update(['done' => true]);
                 }
-                
                 $undeliveredDevicesCount = $order->devices_orders()->where('deliver_to_user', false)->count();
                 $undeliveredProductsCount = $order->products_orders()->where('deliver_to_user', false)->count();
                 if ($undeliveredDevicesCount === 0 && $undeliveredProductsCount === 0) {
